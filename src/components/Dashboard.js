2bcefe03edcd5b6444a6ef73525778288107f1e6
jritu96111 <ritu.jaiswal@thewitslab.com>
2026-03-13 12:59:44 +0530

    removed item two

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -25,7 +25,7 @@ function Sidebar() {
         <li>Settings</li>
         <li>Logout</li>
         <li>Additional Item 1</li>
-        <li>Additional Item 2</li>
+        {/* <li>Additional Item 2</li> */}
         <li>Additional Item 3</li>
         <li>Additional Item 4</li>
       </ul>
